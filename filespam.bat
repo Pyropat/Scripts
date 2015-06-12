5c4b7b39fcd68b1509fad8584a413bc5f691f2ce
Matthew7758 <Matthew7758@users.noreply.github.com>
2015-06-11 21:29:25 -0400

    Update filespam.bat
    This script checks to see if folders exist at C:\test and C:\test\files and creates them if they do not. It then checks for and creates a file called "file.txt" with the text "Hello World!" written in it, then proceeds to copy and

--- filespam.bat
+++ filespam.bat
@@ -4,10 +4,10 @@ set source=C:\test
 set destination=C:\test\files
 set cfile=C:\test\files\file.txt
 if not exist %source% md %source%
-if not exist %file% @echo "Hello World!" > %file%
 if not exist %destination% md %destination%
+if not exist %file% @echo "Hello World!" > %file%
 goto spam
 :spam
 copy %file% %destination%
-ren %cfile% "file-%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%.txt"
+ren %cfile% "file-%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%.txt"
 goto spam
